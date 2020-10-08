9647515b0420a7446bd80460bdb0e188bd0a3418
Guillaume Smet <guillaume.smet@gmail.com>
2020-10-08 19:57:00 +0200

    Add some Caffeine classes for reflection

--- src/main/java/io/quarkus/status/CacheNames.java
+++ src/main/java/io/quarkus/status/CacheNames.java
@@ -1,5 +1,11 @@
 package io.quarkus.status;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(classNames = {
+        "com.github.benmanes.caffeine.cache.SSW",
+        "com.github.benmanes.caffeine.cache.PSW"
+})
 public interface CacheNames {
 
     String STATUS_CACHE_NAME = "status";
