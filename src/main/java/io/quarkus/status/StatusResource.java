82ae6897c9be9a177f5429914693115d0977c0c6
George Gastaldi <gegastaldi@gmail.com>
2021-03-10 23:44:10 -0300

    Use Quarkiverse PrettyTime extension

--- src/main/java/io/quarkus/status/StatusResource.java
+++ src/main/java/io/quarkus/status/StatusResource.java
@@ -1,9 +1,6 @@
 package io.quarkus.status;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -11,12 +8,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import io.quarkus.qute.TemplateExtension;
 import io.quarkus.qute.TemplateInstance;
 import io.quarkus.qute.api.CheckedTemplate;
 import io.quarkus.status.model.Stats;
 import io.quarkus.status.model.Status;
-import org.ocpsoft.prettytime.PrettyTime;
 
 @Path("/")
 public class StatusResource {
@@ -72,14 +67,5 @@ public class StatusResource {
         StringBuilder sb = new StringBuilder();
         labelsService.getEnhancementsLabels().forEach( label -> sb.append(label).append("\n"));
         return sb.toString();
-    }
-
-    @TemplateExtension
-    static class Extensions {
-
-        static String formatDateTime(LocalDateTime dateTime) {
-            PrettyTime p = new PrettyTime();
-            return p.format(Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant()));
-        }
     }
 }
