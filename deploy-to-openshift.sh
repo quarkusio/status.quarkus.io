776773f638890fffb4f8c8f720c16cd612d43749
Guillaume Smet <guillaume.smet@gmail.com>
2021-03-23 17:21:21 +0100

    Add information on how to recover from a manifest error on OpenShift

--- deploy-to-openshift.sh
+++ deploy-to-openshift.sh
@@ -8,3 +8,9 @@ oc project prod-status-quarkus-io
 mvn clean package -Dquarkus.kubernetes.deploy=true -Dquarkus.native.container-build=true -Dnative
 
 # add kubernetes.io/tls-acme: 'true' to the route to renew the SSL certificate automatically
+
+# list the available image streams
+# oc get is
+# clean up the image streams in case of manifest errors
+# oc delete is ubi-quarkus-native-s2i
+# oc delete is ubi-quarkus-native-binary-s2i
